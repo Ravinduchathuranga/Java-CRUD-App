7656978e0b868c28367efd5fcdeeb4e8dfd86f40
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2024-07-01 14:58:47 +0530

    log in and register snipits updated!

--- src/Appinitializer.java
+++ src/Appinitializer.java
@@ -1,3 +1,36 @@
-public class Appinitializer {
+import java.util.Scanner;
 
+public class Appinitializer {
+    public static void main(String[] args) {
+        //database area
+        String[][]users=new String[3][2];
+        Scanner scanner = new Scanner(System.in);
+        String[] initializePageQuestions = {
+                "1) Do you want to login?",
+                "2) Are you new to here?",
+                "3) Do you want to exit the page?",
+        };
+        for (String question : initializePageQuestions) {
+            System.out.println(question);
+        }
+        int userInput = input.nextInt();
+        switch (userInput) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                return;
+        }
+    }
+//    logInprocess
+    public static boolean login(){
+
+    }
+//    register process
+    public static boolean register(){
+
+    }
 }
